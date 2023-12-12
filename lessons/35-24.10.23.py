43fd3fbcd53ccfda143cd9088dfb3a1f99fa09a8
Kalyonov <kalyonov74742000@mail.ru>
2023-12-12 14:53:22 +0400

    dz_35

---
+++ lessons/35-24.10.23.py
@@ -0,0 +1,25 @@
+
+import sqlite3
+
+# con = sqlite3.connect('profile.db')
+# cur = con.cursor()
+#
+# cur.execute('''
+# ''')
+#
+# con.close()
+
+
+with sqlite3.connect('profile.db') as con:
+    cur = con.cursor()
+    # cur.execute('''CREATE TABLE IF NOT EXISTS users(
+    #     id INTEGER PRIMARY KEY AUTOINCREMENT,
+    #     name TEXT NOT NULL,
+    #     summa REAL,
+    #     data BLOB
+    # )''')
+    cur.execute('DROP TABLE users')
+
+print('Hello')
+
+
